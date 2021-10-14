4b8828763826ee32313c4c2dd93cc3c5425c7a64
achapawa <77375149+achapawa@users.noreply.github.com>
2021-10-13 22:08:02 -0600

    JavaFX loads a connction screen

--- client/src/main/java/Client/MonopolyClient.java
+++ client/src/main/java/Client/MonopolyClient.java
@@ -15,5 +15,28 @@ public class MonopolyClient extends Application {
 
     @Override
     public void start(Stage gameStage) throws Exception {
+
+        gameStage.setTitle("Connection Box");
+        GridPane box=new GridPane();
+        TextField address=new TextField();
+        TextField port_value=new TextField();
+        Button connect=new Button("Connect");
+        box.add(connect,4,4);
+        box.add(port_value,5,2);
+        box.add(address,2,2);
+        Scene scene=new Scene(box,500,100);
+        gameStage.setScene(scene);
+        gameStage.show();
+
+        String str_ad=address.toString();
+        String str_port=address.toString();
+
+        Connection connection=new Connection();
+        connection.initialize(str_ad,str_port);
+
+        if (connection.isInitialize()){
+            System.out.println("Successful in connecting to the server");
+        }
     }
+
 }

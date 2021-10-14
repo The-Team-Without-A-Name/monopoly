71b3da034806e3aab885c2728bfbd59dfdd19037
achapawa <77375149+achapawa@users.noreply.github.com>
2021-10-13 22:00:42 -0600

    Module info added, initialized the connection of client to server

--- client/src/main/java/Client/MonopolyClient.java
+++ client/src/main/java/Client/MonopolyClient.java
@@ -1,13 +1,19 @@
 package Client;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MonopolyClient extends Application {
 
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
 
     @Override
     public void start(Stage gameStage) throws Exception {
-
     }
 }

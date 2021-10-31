28442c71f49d48369cb9ec5a4d3a1c2f0130865e
achapawa <77375149+achapawa@users.noreply.github.com>
2021-10-30 18:42:08 -0600

    Added Player, and Spaces classes, Updated Dice Class

--- library/src/main/java/library/Dice.java
+++ library/src/main/java/library/Dice.java
@@ -1,5 +1,47 @@
 package library;
 
+import java.util.Random;
+
+//this class handles dice
 public class Dice {
+
+    private  int diceTotal;
+    private  int faceValue1;
+    private  int faceValue2;
+    private Random randNum=new Random();
+    private boolean db;
+
+
+   // constructor for dice object
+    public Dice(){
+
+    }
+
+    //get sum of the faces of this roll
+    public int getDiceTotal() {
+        return diceTotal;
+    }
+
+    public int getFaceValue1() {
+        return faceValue1;
+    }
+
+    public int getFaceValue2() {
+        return faceValue2;
+    }
+
+    //method for rolling dice
+    public void roll(){
+
+        //this method should have some functionality to change the db(boolean) when two facevalues are equal.
+    }
+
+    //returns true if last roll was double
+    public boolean isDouble(){
+        return db;
+    }
+
+
+
 
 }

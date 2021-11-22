24795d456817aa1bc7cc9ef2ac34c0d253f55a98
idahodruid <77563073+idahodruid@users.noreply.github.com>
2021-11-22 05:46:47 -0700

    Removed unneeded spaces, added dockerfile
    added dockerfile and removed/moved unneeded space classes into space

--- library/src/main/java/library/TaxSpaces.java
+++
@@ -1,6 +0,0 @@
-package library;
-/**
- * handles income tax space and Luxury tax space Player must pay 200 or 10% of assets for income tax
- * or pay 100 for luxury tax space upon landing.
- */
-public class TaxSpaces {}
